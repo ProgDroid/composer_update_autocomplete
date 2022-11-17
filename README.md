d3c387dc054dbd9575c8df8afa73186eae270efe
Fernando Ferreira <fm_ferreira1996@hotmail.com>
2022-11-17 18:57:15 +0000

    Create Composer Update Autocomplete

---
+++ README.md
@@ -0,0 +1,19 @@
+# Composer Update Autocomplete for Bash
+
+This small tool allows using Bash autocompletion (with `<TAB><TAB>`) when updating Composer modules.
+
+It first tries to find the `composer.json` file on the current working directory, then searches each non-hidden child directory (in parallel) until it finds one. It does **not** recursively search the child directories.
+
+## Why
+
+I was tired of typing very long and easy to misspell module names at work.
+
+## How (to use)
+
+- Download the binary
+- Add the following lines to your `.bashrc` file:
+```bash
+complete -C /path/to/binary docker-compose run --rm composer update # add this if you (like me) run composer inside a docker container
+complete -C /path/to/binary docker-compose run composer update # add this in case you forget to do --rm
+complete -C /path/to/binary composer update # add this if you use a composer installation (or in my case, an alias for the above)
+```
---
+++ src/main.rs
@@ -0,0 +1,123 @@
+use std::{borrow::ToOwned, env, fs, path::Path};
+
+use anyhow::{anyhow, Result};
+use rayon::prelude::*;
+use serde::Deserialize;
+use serde_json::Value;
+use walkdir::{DirEntry, WalkDir};
+
+const ARGS_ENV_VAR: &str = "COMP_LINE";
+const CURSOR_POSITION_ENV_VAR: &str = "COMP_POINT";
+const COMPOSER_FILE_NAME: &str = "composer.json";
+
+#[derive(Deserialize, Debug)]
+struct ComposerModules {
+    require: Value,
+    #[serde(rename = "require-dev")]
+    require_dev: Value,
+}
+
+fn get_current_arg() -> Option<String> {
+    let cursor_position: usize = env::var(CURSOR_POSITION_ENV_VAR).ok()?.parse().ok()?;
+    let args: String = env::var(ARGS_ENV_VAR)
+        .ok()?
+        .chars()
+        .take(cursor_position)
+        .collect();
+
+    args.split_whitespace().map(ToOwned::to_owned).last()
+}
+
+fn is_hidden(entry: &DirEntry) -> bool {
+    entry
+        .file_name()
+        .to_str()
+        .map_or(false, |s| s.starts_with('.'))
+}
+
+fn get_composer_location() -> Result<String> {
+    let current_dir: String = env::current_dir()?.display().to_string();
+    let dir_entry = WalkDir::new(current_dir)
+        .max_depth(1)
+        .into_iter()
+        .filter_entry(|e| !is_hidden(e))
+        .par_bridge()
+        .filter_map(Result::ok)
+        .find_any(|e| {
+            Path::new(
+                format!(
+                    "{}/{}",
+                    e.file_name().to_str().unwrap_or(""),
+                    COMPOSER_FILE_NAME
+                )
+                .as_str(),
+            )
+            .is_file()
+        });
+
+    match dir_entry {
+        Some(entry) => Ok(format!(
+            "{}/{}",
+            entry.file_name().to_str().unwrap_or(""),
+            COMPOSER_FILE_NAME
+        )),
+        None => Err(anyhow!("Could not find composer file")),
+    }
+}
+
+fn get_composer_modules() -> Result<ComposerModules> {
+    let current_dir_composer_file_content = fs::read_to_string(COMPOSER_FILE_NAME);
+    if let Ok(c) = current_dir_composer_file_content {
+        let modules: ComposerModules = serde_json::from_str(&c)?;
+        return Ok(modules);
+    }
+
+    match get_composer_location() {
+        Ok(path) => {
+            let content: String = fs::read_to_string(path)?;
+            let modules: ComposerModules = serde_json::from_str(&content)?;
+            Ok(modules)
+        }
+        Err(e) => Err(e),
+    }
+}
+
+#[allow(clippy::print_stdout)]
+fn main() -> Result<()> {
+    let current_arg = match get_current_arg() {
+        Some(arg) => arg,
+        None => {
+            return Err(anyhow!("Could not get arguments"));
+        }
+    };
+
+    let composer_modules = get_composer_modules()?;
+
+    let require_keys: Vec<String> = match composer_modules.require.as_object() {
+        Some(obj) => obj
+            .keys()
+            .filter(|key| current_arg == "update" || key.starts_with(&current_arg))
+            .cloned()
+            .collect(),
+        None => Vec::new(),
+    };
+
+    let require_dev_keys: Vec<String> = match composer_modules.require_dev.as_object() {
+        Some(obj) => obj
+            .keys()
+            .filter(|key| current_arg == "update" || key.starts_with(&current_arg))
+            .cloned()
+            .collect(),
+        None => Vec::new(),
+    };
+
+    for key in require_keys {
+        println!("{}", key);
+    }
+
+    for key in require_dev_keys {
+        println!("{}", key);
+    }
+
+    Ok(())
+}
